1576a031eb94d2d09048c6a2206bbc45e1d2a2b5
cat3a <cat3a-88@mail.ru>
2023-07-15 01:25:00 +0300

    Добавляет страницу 404. Начинает описывать маршруты в приложении.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,34 @@
 import Main from '../../pages/main/main';
+import NotFound from '../../pages/not-found/not-found';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 type AppProps = {
   rentalOffers: number;
 };
 
 function App({ rentalOffers }: AppProps): JSX.Element {
-  return <Main rentalOffers={rentalOffers} />;
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Main rentalOffers={rentalOffers} />} />
+            <Route path="login" element={<Login />}></Route>
+            <Route path="favorites" element={<Favorites />}></Route>
+            <Route path="offer">
+              <Route index element={<Offer />} />
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
 export default App;
