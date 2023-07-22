31fcc1fbce590dfd413a7c2bba81622345043051
cat3a <cat3a-88@mail.ru>
2023-07-23 01:23:16 +0300

    Приводит карточку предложения в работоспособное состояние

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,51 +1,67 @@
 import { ApartmentOffer } from '../../types/offer';
 
 type CardProps = {
-  offers: ApartmentOffer[];
+  offer: ApartmentOffer;
 };
 
-//TODO: Доделать карточку.
-
-function Card({offers}: CardProps): JSX.Element {
+function Card({ offer}: CardProps): JSX.Element {
   return (
-    <article key={offer.id} className="cities__card place-card">
-      {offer.isPremium ? <div className="place-card__mark"><span>isPremium</span></div> : ''}
-      <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img
-            className="place-card__image"
-            src={offer.previewImage}
-            width="260"
-            height="200"
-            alt={offer.title}
-          />
-        </a>
-      </div>
-      <div className="place-card__info">
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{offer.price}</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
+    <div key={offer.id}>
+      <article key={offer.id} className="cities__card place-card">
+        {offer.isPremium ? (
+          <div className="place-card__mark">
+            <span>isPremium</span>
           </div>
-          <button className={offer.isFavorite ? 'place-card__bookmark-button place-card__bookmark-button--active button' : 'place-card__bookmark-button button'} type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"></use>
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
+        ) : (
+          ''
+        )}
+        <div className="cities__image-wrapper place-card__image-wrapper">
+          <a href="#">
+            <img
+              className="place-card__image"
+              src={offer.previewImage}
+              width="260"
+              height="200"
+              alt={offer.title}
+            />
+          </a>
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{width:`${(offer.rating * 20).toString()}%`}}></span>
-            <span className="visually-hidden">Rating</span>
+        <div className="place-card__info">
+          <div className="place-card__price-wrapper">
+            <div className="place-card__price">
+              <b className="place-card__price-value">&euro;{offer.price}</b>
+              <span className="place-card__price-text">&#47;&nbsp;night</span>
+            </div>
+            <button
+              className={
+                offer.isFavorite
+                  ? 'place-card__bookmark-button place-card__bookmark-button--active button'
+                  : 'place-card__bookmark-button button'
+              }
+              type="button"
+            >
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref="#icon-bookmark"></use>
+              </svg>
+              <span className="visually-hidden">To bookmarks</span>
+            </button>
           </div>
+          <div className="place-card__rating rating">
+            <div className="place-card__stars rating__stars">
+              <span
+                style={{ width: `${(offer.rating * 20).toString()}%` }}
+              >
+              </span>
+              <span className="visually-hidden">Rating</span>
+            </div>
+          </div>
+          <h2 className="place-card__name">
+            <a href="#">{offer.title}</a>
+          </h2>
+          <p className="place-card__type">{offer.type}</p>
         </div>
-        <h2 className="place-card__name">
-          <a href="#">{offer.title}</a>
-        </h2>
-        <p className="place-card__type">{offer.type}</p>
-      </div>
-    </article>
+      </article>
+    </div>
   );
 }
 
--- src/components/offer-list/offer-list.tsx
+++ src/components/offer-list/offer-list.tsx
@@ -1,12 +1,12 @@
-/*import Card from '../../components/card/card'; */
-import {ApartmentOffer} from '../../types/offer';
+import Card from '../../components/card/card';
+import { ApartmentOffer } from '../../types/offer';
 
 type OfferListProps = {
   rentalOffers: number;
   offers: ApartmentOffer[];
 };
 
-function OfferList({rentalOffers, offers}: OfferListProps) {
+function OfferList({ rentalOffers, offers }: OfferListProps) {
   return (
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
@@ -38,45 +38,8 @@ function OfferList({rentalOffers, offers}: OfferListProps) {
       </form>
       <div className="cities__places-list places__list tabs__content">
         {offers.map((offer) => (
-          <article key={offer.id} className="cities__card place-card">
-            {offer.isPremium ? <div className="place-card__mark"><span>isPremium</span></div> : ''}
-            <div className="cities__image-wrapper place-card__image-wrapper">
-              <a href="#">
-                <img
-                  className="place-card__image"
-                  src={offer.previewImage}
-                  width="260"
-                  height="200"
-                  alt={offer.title}
-                />
-              </a>
-            </div>
-            <div className="place-card__info">
-              <div className="place-card__price-wrapper">
-                <div className="place-card__price">
-                  <b className="place-card__price-value">&euro;{offer.price}</b>
-                  <span className="place-card__price-text">&#47;&nbsp;night</span>
-                </div>
-                <button className={offer.isFavorite ? 'place-card__bookmark-button place-card__bookmark-button--active button' : 'place-card__bookmark-button button'} type="button">
-                  <svg className="place-card__bookmark-icon" width="18" height="19">
-                    <use xlinkHref="#icon-bookmark"></use>
-                  </svg>
-                  <span className="visually-hidden">To bookmarks</span>
-                </button>
-              </div>
-              <div className="place-card__rating rating">
-                <div className="place-card__stars rating__stars">
-                  <span style={{width:`${(offer.rating * 20).toString()}%`}}></span>
-                  <span className="visually-hidden">Rating</span>
-                </div>
-              </div>
-              <h2 className="place-card__name">
-                <a href="#">{offer.title}</a>
-              </h2>
-              <p className="place-card__type">{offer.type}</p>
-            </div>
-          </article>
-        ))}
+          <Card offer={offer} key={offer.id} />
+        ))};
       </div>
     </section>
   );
