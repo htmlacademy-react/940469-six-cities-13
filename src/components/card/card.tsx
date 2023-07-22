46d583620181590bf8e79aeb759046a59b586163
cat3a <cat3a-88@mail.ru>
2023-07-22 15:32:50 +0300

    Подготавливает карточку предложения для получения данных через props

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,24 +1,32 @@
-function Card(): JSX.Element {
+import { ApartmentOffer } from '../../types/offer';
+
+type CardProps = {
+  offerCard: ApartmentOffer;
+};
+
+//TODO: Доделать премиум, избранное и рейтинг.
+
+function Card({offerCard}: CardProps): JSX.Element {
   return (
     <article className="cities__card place-card">
       <div className="place-card__mark">
-        <span>Premium</span>
+        <span>isPremium</span>
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
           <img
             className="place-card__image"
-            src="img/apartment-01.jpg"
+            src={offerCard.previewImage}
             width="260"
             height="200"
-            alt="Place image"
+            alt={offerCard.title}
           />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{offerCard.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -35,9 +43,9 @@ function Card(): JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{offerCard.title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{offerCard.type}</p>
       </div>
     </article>
   );
