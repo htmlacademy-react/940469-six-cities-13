ce6f65b2d8e9a22ac7ef1bd841d65db7f06066b5
cat3a <cat3a-88@mail.ru>
2023-08-10 23:40:01 +0300

    Добавляет toggle

--- src/components/sorting/sorting.tsx
+++ src/components/sorting/sorting.tsx
@@ -1,7 +1,8 @@
-import { useAppDispatch} from '../../hooks';
+import { useAppDispatch } from '../../hooks';
 import { sorting } from '../../const';
 import { changeSorting } from '../../store/action';
 import cn from 'classnames';
+import { useState } from 'react';
 
 type SortingProps = {
   activeSorting: string;
@@ -9,16 +10,25 @@ type SortingProps = {
 
 export default function Sorting({ activeSorting }: SortingProps): JSX.Element {
   const dispatch = useAppDispatch();
+  const [toggle, setToggle] = useState(false);
 
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={() => setToggle(!toggle)}
+      >
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul
+        className={cn('places__options places__options--custom', {
+          'places__options--opened': toggle,
+        })}
+      >
         {sorting.map((sort) => (
           <li
             key={sort}
